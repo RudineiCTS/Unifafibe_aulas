a3eeae40710967c7bb1a25ea4602d453845427e2
Rudinei Augusto Costa <47544016+Jooker918@users.noreply.github.com>
2019-04-01 22:31:34 -0300

    Add files via upload

---
+++ Pilha_ponteiro.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+#include "pilha.h"
+
+void main()
+{
+	/*disponibilizar os seguintes recursos:
+	1- Criar Pilha
+	2- asicionar elemnto na pilha
+	3- remover todos os elemntos (exibindo-os)
+	4- remover um elemento da pilha
+	5- remover Pilha
+	6- encerrar
+	*/
+	int escolha;
+	char valor;
+	int flag = 1;
+	Pilha* r = 0;
+
+	while (flag == 1) {
+	cout << "1- Criar Pilha " << endl;
+	cout << "2- adicionar elemnto na pilha" << endl;
+	cout << "3- remover todos os elemntos (exibindo-os)" << endl;
+	cout << "4- remover um elemento da pilha " << endl;
+	cout << "5- remover Pilha" << endl;
+	cout << "6- encerrar" << endl;
+	cin >> escolha;
+
+	
+	
+		switch (escolha)
+		{
+		case 1:
+			r = cria();
+			
+
+		
+			break;
+		case 2:
+			cout << "digite um numero para inserir na pilha" << endl;
+			cin >> valor;
+			push(r, valor);
+
+
+
+			break;
+		case 3:
+			while (!vazia(r)) {
+				cout << pop(r) << endl;
+			}
+
+
+			break;
+		case 4:
+			cout << pop(r);
+
+			break;
+		case 5:
+			liberar(r);
+
+			break;
+		case 6:
+			
+			flag = 2;
+			
+
+			break;
+		}
+}
+	cout << "programa encerrado" << endl;
+
+}
---
+++ pilha.h
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+
+typedef struct
+{
+	int topo;
+	char vetor[100];
+
+}Pilha;
+
+//Criar nova pilha
+
+Pilha* cria()
+{
+	//p= malloc(sizeof(Pilha))
+	Pilha* p = new Pilha();
+	p->topo = 0;
+
+	return p;
+}
+
+//verificar se a pilha esta cheia 
+int cheia(Pilha* p)
+{
+	return (p->topo == 100);
+}
+
+// vereifica se apilha está vazia 
+int vazia(Pilha* p)
+{
+	return(p->topo == 0);
+}
+
+//inserir novo valor na PILHA
+//retorna mensagem se a pilha estiver cheia
+
+void push(Pilha* p,char valor)
+{
+	if (!cheia(p))
+	{
+		p->vetor[p->topo] = valor;
+		p->topo++;
+	}
+	else {
+		cout << "Pilha está cheia " << endl;
+		exit(1);
+	}
+
+}
+//tarefa
+//criar a função para remover um item da PILHA
+//retornar mensagem se pilha estiver vazia
+char pop(Pilha* p)
+{
+	char elem;
+	if (vazia(p)) {
+		cout << "impossivel remover valor, PILHA VAZIA :" << endl;
+		exit(1);
+	}
+	
+	elem = p->vetor[p->topo - 1];
+	p->topo--;
+	return elem;
+
+}
+
+void liberar(Pilha* p)
+{
+	delete p;
+}
